8d0c6312b761bc2dd334ea4178758abc95de9363
Cedric Martens <cedricmartens98@gmail.com>
2017-04-01 18:30:47 -0400

    Fixed floating problem

--- Assets/Scripts/FloatingAnimation.cs
+++ Assets/Scripts/FloatingAnimation.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class FloatingAnimation : MonoBehaviour {
+    public float startY;
 
 	void Start () {
-		
+        if (startY == null)
+            startY = gameObject.transform.position.y;
 	}
 
     void Update()
@@ -14,7 +16,7 @@ public class FloatingAnimation : MonoBehaviour {
 
         gameObject.transform.position = new Vector3(
             gameObject.transform.position.x,
-            Mathf.Sin(Time.time)/2f + 1.5f,
+            Mathf.Sin(Time.time)/2f + startY,
             gameObject.transform.position.z);
     }
 }
