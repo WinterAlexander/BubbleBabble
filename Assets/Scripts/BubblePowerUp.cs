b7c644d69c248eeb581fdf1adf6e60260d7bbc08
Alexander Winter <a.w1nter@hotmail.com>
2017-04-01 17:23:23 -0400

    Setup map

--- Assets/Scripts/BubblePowerUp.cs
+++ Assets/Scripts/BubblePowerUp.cs
@@ -20,7 +20,6 @@ public class BubblePowerUp : MonoBehaviour {
         {
             case PowerUpType.GIANT_BUBBLE :
                 gameObject.transform.localScale = new Vector3(2, 2, 2);
-                Debug.Log("Big!");
                 break;
         }
 	}
