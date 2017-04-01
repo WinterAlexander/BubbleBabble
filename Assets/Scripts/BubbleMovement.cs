80c94dae107808edf3e0ab65eee79231ccbfb42e
Alexander Winter <a.w1nter@hotmail.com>
2017-04-01 10:12:39 -0400

    Added 4 bulles

--- Assets/Scripts/BubbleMovement.cs
+++ Assets/Scripts/BubbleMovement.cs
@@ -1,20 +1,35 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class BubbleMovement : MonoBehaviour
 {
 	public float speed = 6.0F;
-	private Vector2 moveDirection = new Vector2();
+	public int controllerId = 1;
+
+	private Vector3 moveDirection = new Vector3();
+	private float distToGround;
+
+	void Start()
+	{
+		distToGround = GetComponent<Collider>().bounds.extents.y;
+	}
 
 	void Update()
 	{
-		Rigidbody2D body = GetComponent<Rigidbody2D>();
 
-		moveDirection.Set(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+		Rigidbody body = GetComponent<Rigidbody>();
+
+		moveDirection.Set(Input.GetAxis("Horizontal_" + controllerId), 0, Input.GetAxis("Vertical_" + controllerId));
 		moveDirection = transform.TransformDirection(moveDirection);
 		moveDirection *= speed;
 
 		body.AddForce(moveDirection);
+	}
+
+	bool isOnGround()
+	{
+		return Physics.Raycast(transform.position, -Vector3.up, distToGround + 0.1f);
 	}
 }
