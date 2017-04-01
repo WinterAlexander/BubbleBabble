c27b26033ab73f4eedbea4d759a92da62856a885
Cedric Martens <cedricmartens98@gmail.com>
2017-04-01 16:02:26 -0400

    Added powerup

---
+++ Assets/FloatingAnimation.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloatingAnimation : MonoBehaviour {
+
+	void Start () {
+		
+	}
+
+    void Update()
+    {
+        gameObject.transform.Rotate(1, 1, 0);
+
+        gameObject.transform.position = new Vector3(
+            gameObject.transform.position.x,
+            Mathf.Sin(Time.time)/2f + 1.5f,
+            gameObject.transform.position.z);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Assets/PowerUp.cs
@@ -0,0 +1,36 @@
+﻿using Assets;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUp : MonoBehaviour {
+
+    public PowerUpType type;
+    
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        ArrayList players = GameObject.FindWithTag("WorldController").GetComponent<CheckAlives>().GetPlayers();
+        foreach(GameObject p in players)
+        {
+            float dis = XZDist(p.transform.position, gameObject.transform.position);
+            SphereCollider collider = p.GetComponent<SphereCollider>();
+
+            if (dis < collider.radius)
+            {
+                GameObject.Destroy(gameObject);
+            }
+        }
+    }
+
+    private float XZDist(Vector3 a, Vector3 b)
+    {
+        return Mathf.Sqrt(
+            (a.x - b.x) * (a.x - b.x) +
+            (a.z - b.z) * (a.z - b.z)
+            );
+    }
+}
