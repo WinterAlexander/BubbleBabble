f3b601b177baba7d9d4f2943e781c0d08f540bc3
Alexander Winter <a.w1nter@hotmail.com>
2017-04-01 09:13:08 -0400

    Added bulles behavior

---
+++ Assets/Scripts/BubbleMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BubbleMovement : MonoBehaviour
+{
+	public float speed = 6.0F;
+	private Vector2 moveDirection = new Vector2();
+
+	void Update()
+	{
+		Rigidbody2D body = GetComponent<Rigidbody2D>();
+
+		moveDirection.Set(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+		moveDirection = transform.TransformDirection(moveDirection);
+		moveDirection *= speed;
+
+		body.AddForce(moveDirection);
+	}
+}
