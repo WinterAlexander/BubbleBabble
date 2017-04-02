cc28081a65dc89e81da77226d043c0aa0d709623
Cedric Martens <cedricmartens98@gmail.com>
2017-04-02 13:58:13 -0400

    Freeze effect when someone wins and goes to menu after 5 seconds

--- Assets/Scripts/CheckAlives.cs
+++ Assets/Scripts/CheckAlives.cs
@@ -1,11 +1,14 @@
-﻿using System.Collections;
+﻿using Assets.Scripts;
+using Assets.Scripts.Effects;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CheckAlives : MonoBehaviour
 {
 	public float deadHeight = -2;
-
+    public bool finished = false;
 	private ArrayList players;
 
 	void Start()
@@ -15,6 +18,9 @@ public class CheckAlives : MonoBehaviour
 
 	void Update()
 	{
+        if (finished)
+            return;
+
 		for(int i = 0; i < players.Count; i++)
 		{
 			GameObject player = players[i] as GameObject;
@@ -36,8 +42,11 @@ public class CheckAlives : MonoBehaviour
 
 	public void Win(GameObject winner)
 	{
-		
-
+        finished = true;
+        Freeze.FreezeFrame(5f);
+        Freeze.FreezeComponent fc = Camera.main.gameObject.GetComponent<Freeze.FreezeComponent>();
+        if(fc.action == null)
+            fc.action = new GoToMenuAction();                
 	}
 
 	public ArrayList GetPlayers()
