2c178d7328792971de959de76aa8923fff52aa7e
Cedric Martens <cedricmartens98@gmail.com>
2017-04-02 14:49:52 -0400

    Added finish animation

--- Assets/Scripts/CheckAlives.cs
+++ Assets/Scripts/CheckAlives.cs
@@ -4,16 +4,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class CheckAlives : MonoBehaviour
 {
-	public float deadHeight = -2;
+    public float deadHeight = -2;
     public bool finished = false;
-	private ArrayList players;
+    public string winningText = "Joueur {0} wins!";
+    private ArrayList players;
+    private GameObject[] playerId;
 
 	void Start()
 	{
 		players = new ArrayList(GameObject.FindGameObjectsWithTag("Player"));
+        playerId = new GameObject[players.Count];
+        for(int i = 0; i < players.Count; i++)
+        {
+            playerId[i] = (GameObject)players[i];
+        }
 	}
 
 	void Update()
@@ -33,20 +41,33 @@ public class CheckAlives : MonoBehaviour
 
 				if(players.Count == 1)
 				{
-					Win(players[0] as GameObject);
-					return;
+                    for(int j = 0; j < playerId.Length; j++)
+                    {
+                        if (playerId[j].Equals(players[0]))
+                        {
+                            Win(j + 1);
+                            return;
+                        }
+                    }					
 				}
 			}
 		}
 	}
 
-	public void Win(GameObject winner)
+	public void Win(int winner)
 	{
         finished = true;
+
         Freeze.FreezeFrame(5f);
         Freeze.FreezeComponent fc = Camera.main.gameObject.GetComponent<Freeze.FreezeComponent>();
         if(fc.action == null)
-            fc.action = new GoToMenuAction();                
+        {
+            fc.action = new GoToMenuAction();
+            GameObject winnerCanvas = GameObject.Find("WinnerCanvas");
+            Text text = winnerCanvas.GetComponentInChildren<Text>();
+            text.text = string.Format(winningText, winner);
+        }
+                            
 	}
 
 	public ArrayList GetPlayers()
