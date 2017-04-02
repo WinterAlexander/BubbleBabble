7243d728a6122c9e2c11ca82d45d8e2dd031101d
Cedric Martens <cedricmartens98@gmail.com>
2017-04-02 13:15:58 -0400

    Added alot of new prefabs and a tutorial level

---
+++ Assets/Scripts/FollowPlayerCamera.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayerCamera : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        GameObject player = GameObject.Find("Bulle Player");
+        Vector3 playerV = player.transform.position;
+        gameObject.transform.position = new Vector3(playerV.x, gameObject.transform.position.y, gameObject.transform.position.z);
+	}
+}
